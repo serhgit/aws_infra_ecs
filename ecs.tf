21c25068191509266b66a6e8306f0e0cf0af77ef
Serhii P <e-mail@e-mail.com>
2021-09-13 12:21:23 +0300

    Added ssh-key

--- ecs.tf
+++ ecs.tf
@@ -17,7 +17,7 @@ data "aws_availability_zones" "available" {
 
 resource "aws_key_pair" "ecs" {
   key_name   = "${var.environment}-${var.environment}-ecs-key"
-  public_key = file("~/Documents/aws/aws_keys/id_rsa_ec2.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkb4KvVBYk31b8mQ5hqBWy9kLXTA2awN+yyu8FzJHeqZkwxF7XoXbMYvZ6xZ/H8Py9HRfNDQpIirJ1rmargYaWTXnl4peqMUxcIg2F/nlZUB8AKMjIFmbDnv1nvuvAdOz1XgreFIsguhzqG5Bv3kZ7xCZ4pNsq1xhv2+OOTmHzrvQu1H+hid9Or42MoWJb98neapTvIrkwW2VhbmvhSEueNihsry54TEungNvRquI7XCtfQJht8WC/W5PiMriuPi1orlJI7OZjcP3R6sSDk59Aukbet9ntFse/KwFn+gUgInjCnqJ+12eq1ZJZCYmJPFvsPzauV3tnRxkiSq2DYQERaJUZ8rIsSjT0ynAmSuAUwYdYnangGh8Q4FNGRK5fv4uTpLnRlk7yrJtqJlCuLBoDaWs1FLVvO3fajzZrHu2CpeDUCvfnUrOlc9MwGM68JO4OZNCmp868bNvQFvPcEQgX3QC2YWIQ3va/+m7+ESVlvBNWa7Aht26eQD9k2sxCdEM= EPAM+Serhii_Protsun@EPUAKYIW13FA" 
 }
 
 module "ecs" {
